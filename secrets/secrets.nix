4a254ad2c74b33181d0b392bf73028aec85bdf15
Daniel Firth <dan.firth@homotopic.tech>
2024-09-17 12:01:47 +0000

    rekey with noon

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,11 +2,13 @@ let
 
   earthrealm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEMuBv9vXsKsOsjS7B6zMOpuLw+gGGHR6hADuTeiNfKO lc@earthrealm";
 
+  noon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIATz/Jv+AnBft+9Q01UF07OydvgTTaTdCa+nMqabkUNl";
+
   aws = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID0sPEGJz6opbVgbjq82PQawphIGBzJjWNwJ3VGQFpMr root@ip-172-31-6-250.eu-west-3.compute.internal";
 
   systems = [aws];
 
-  users = [earthrealm];
+  users = [earthrealm noon];
 in
 {
 
